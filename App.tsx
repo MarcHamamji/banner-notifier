9d7803e3ed74d64e25c904321f6e49de7754cd42
MarcHamamji <hamamjimarc@gmail.com>
2024-06-26 13:58:59 +0300

    Use react-navigation for tab management

--- App.tsx
+++ App.tsx
@@ -4,16 +4,19 @@ import logsStore from './src/stores/logs';
 import settingsStore from './src/stores/settings';
 import Main from './src/Main';
 import filtersStore from './src/stores/filter';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
   return (
-    <logsStore.Provider>
-      <settingsStore.Provider>
-        <filtersStore.Provider>
-          <Main />
-        </filtersStore.Provider>
-      </settingsStore.Provider>
-    </logsStore.Provider>
+    <NavigationContainer>
+      <logsStore.Provider>
+        <settingsStore.Provider>
+          <filtersStore.Provider>
+            <Main />
+          </filtersStore.Provider>
+        </settingsStore.Provider>
+      </logsStore.Provider>
+    </NavigationContainer>
   );
 }
 
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,8 @@
 import React, {useMemo} from 'react';
 
-import {useCallback, useState} from 'react';
-import {
-  ActivityIndicator,
-  Appbar,
-  BottomNavigation,
-  PaperProvider,
-} from 'react-native-paper';
-import {BaseRoute} from 'react-native-paper/lib/typescript/components/BottomNavigation/BottomNavigation';
+import {useCallback} from 'react';
+import {ActivityIndicator, Appbar, PaperProvider} from 'react-native-paper';
+import {createMaterialBottomTabNavigator} from 'react-native-paper/react-navigation';
 
 import HomeRoute from '../src/routes/HomeRoute';
 import LogsRoute from '../src/routes/LogsRoute';
@@ -16,54 +11,15 @@ import logsStore from './stores/logs';
 import settingsStore from './stores/settings';
 import filtersStore from './stores/filter';
 
+const Tab = createMaterialBottomTabNavigator();
+
 function Main(): React.JSX.Element {
   const badgeNeeded = logsStore.useStoreState(state => state.badgeNeeded);
   const markAllAsSeen = logsStore.useStoreActions(state => state.markAllAsSeen);
 
-  const [index, setIndex] = useState(0);
-  const [routes] = useState<BaseRoute[]>([
-    {
-      key: 'home',
-      title: 'Home',
-      focusedIcon: 'home',
-    },
-    {
-      key: 'logs',
-      title: 'Logs',
-      focusedIcon: 'list-status',
-    },
-    {
-      key: 'settings',
-      title: 'Settings',
-      focusedIcon: 'cog',
-    },
-  ]);
-
-  const getBadges = useCallback(
-    ({route}: {route: BaseRoute}) => {
-      if (route.key !== 'logs') {
-        return false;
-      }
-      return badgeNeeded;
-    },
-    [badgeNeeded],
-  );
-
-  const renderScene = BottomNavigation.SceneMap({
-    home: HomeRoute,
-    logs: LogsRoute,
-    settings: SettingsRoute,
-  });
-
-  const onIndexChange = useCallback(
-    (newIndex: number) => {
-      if (index === 1) {
-        markAllAsSeen();
-      }
-      setIndex(newIndex);
-    },
-    [index, markAllAsSeen],
-  );
+  const onLogsBlur = useCallback(() => {
+    markAllAsSeen();
+  }, [markAllAsSeen]);
 
   const logsIsRehydrated = logsStore.useStoreRehydrated();
   const settingsIsRehydrated = settingsStore.useStoreRehydrated();
@@ -77,18 +33,36 @@ function Main(): React.JSX.Element {
   return (
     <PaperProvider>
       <Appbar.Header>
-        <Appbar.Content title={routes[index].title} />
+        <Appbar.Content title={'Title'} />
       </Appbar.Header>
       {isRehydrated ? (
-        <BottomNavigation
-          navigationState={{
-            index,
-            routes,
-          }}
-          onIndexChange={onIndexChange}
-          renderScene={renderScene}
-          getBadge={getBadges}
-        />
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeRoute}
+            options={{
+              tabBarIcon: 'home',
+            }}
+          />
+          <Tab.Screen
+            name="Logs"
+            component={LogsRoute}
+            options={{
+              tabBarIcon: 'list-status',
+              tabBarBadge: badgeNeeded,
+            }}
+            listeners={{
+              blur: onLogsBlur,
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsRoute}
+            options={{
+              tabBarIcon: 'cog',
+            }}
+          />
+        </Tab.Navigator>
       ) : (
         <ActivityIndicator
           style={{
